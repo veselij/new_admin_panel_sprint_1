8e921f5d40218a7b1edb7e1d647bf81a998b8a7a
Aleksandr Minaev <veselij85@gmail.com>
2022-03-03 20:36:57 +0300

    fix flake8 errors

--- 02_movies_admin/movies/admin.py
+++ 02_movies_admin/movies/admin.py
@@ -1,33 +1,42 @@
+"""Django admin definitions."""
 from django.contrib import admin
-from .models import Genre, Filmwork, GenreFilmwork, Person, PersonFilmwork
+from movies.models import Filmwork, Genre, GenreFilmwork, Person, PersonFilmwork
 
 
 class GenreFilmworkInline(admin.TabularInline):
+    """Inline class for film genre changing."""
+
     model = GenreFilmwork
 
 
 class PersonFilmworkInline(admin.TabularInline):
+    """Inline class for person film changing."""
+
     model = PersonFilmwork
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ('name', )
-    search_fields = ('name',  'description', 'id')
+    """Admin class for genre with list to display and search."""
+
+    list_display = ("name", )
+    search_fields = ("name", "description", "id")
 
 
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
-    inlines = (GenreFilmworkInline, )
+    """Admin class for filmwork with list to display, search and filters."""
 
-    list_display = ('title', 'type', 'creation_date', 'rating',)
-    list_filter = ('type',)
-    search_fields = ('title', 'description', 'id')
+    inlines = (GenreFilmworkInline, )
+    list_display = ("title", "type", "creation_date", "rating")
+    list_filter = ("type",)
+    search_fields = ("title", "description", "id")
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    inlines = (PersonFilmworkInline, )
+    """Admin class for person with list to display and search."""
 
-    list_display = ('full_name', )
-    search_fields = ('full_name', 'id')
+    inlines = (PersonFilmworkInline, )
+    list_display = ("full_name", )
+    search_fields = ("full_name", "id")
--- 02_movies_admin/movies/models.py
+++ 02_movies_admin/movies/models.py
@@ -1,11 +1,14 @@
+"""Django app movies models definitions."""
 import uuid
 
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.translation import gettext_lazy as _
 
 
 class TimeStampedMixin(models.Model):
+    """Mixin to extend class models with date fields create and modified."""
+
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -14,6 +17,8 @@ class TimeStampedMixin(models.Model):
 
 
 class UUIDMixin(models.Model):
+    """Mixin to extend class models with id uuid field."""
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     class Meta:
@@ -21,6 +26,8 @@ class UUIDMixin(models.Model):
 
 
 class Genre(UUIDMixin, TimeStampedMixin):
+    """Film genre model."""
+
     name = models.CharField(_("name"), max_length=255)
     description = models.TextField(_("description"), blank=True)
 
@@ -31,17 +38,18 @@ class Genre(UUIDMixin, TimeStampedMixin):
 
 
 class Filmwork(UUIDMixin, TimeStampedMixin):
+    """Class model represents model for cinematographic work."""
 
     class FilmType(models.TextChoices):
-        MOVIE = "MV", _("movie")
-        TV_SHOW = "TV", _("tv_show")
+        """Choices for filmwork types."""
+
+        movie = "MV", _("movie")
+        tv_show = "TV", _("tv_show")
 
     title = models.CharField(_("title"), max_length=255)
     description = models.TextField(_("description"))
     creation_date = models.DateField(_("creation_date"), blank=True)
-    rating = models.FloatField(_("rating"), blank=True,
-                                validators=[MinValueValidator(0),
-                                            MaxValueValidator(100)])
+    rating = models.FloatField(_("rating"), blank=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
     type = models.CharField("type", choices=FilmType.choices, max_length=2)
     genres = models.ManyToManyField(Genre, through="GenreFilmwork")
 
@@ -50,11 +58,18 @@ class Filmwork(UUIDMixin, TimeStampedMixin):
         verbose_name = _("filmwork")
         verbose_name_plural = _("filmworks")
 
-    def _str__(self):
+    def __str__(self):
+        """Override default.
+
+        Returns:
+            filmwork title.
+        """
         return self.title
 
 
 class GenreFilmwork(UUIDMixin):
+    """Class model represents relations many to many between genre and filmwork models."""
+
     film_work = models.ForeignKey("Filmwork", on_delete=models.CASCADE)
     genre = models.ForeignKey("Genre", on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
@@ -64,6 +79,8 @@ class GenreFilmwork(UUIDMixin):
 
 
 class Person(UUIDMixin, TimeStampedMixin):
+    """Model for filmwork persons."""
+
     full_name = models.CharField(_("full_name"), max_length=255)
 
     class Meta:
@@ -73,6 +90,8 @@ class Person(UUIDMixin, TimeStampedMixin):
 
 
 class PersonFilmwork(UUIDMixin):
+    """Class model represents relations between person and filmwork models."""
+
     film_work = models.ForeignKey("Filmwork", on_delete=models.CASCADE)
     person = models.ForeignKey("Person", on_delete=models.CASCADE)
     role = models.TextField(_("role"), null=True)
